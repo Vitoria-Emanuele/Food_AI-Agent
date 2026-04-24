c7a0409667123ac6870ca9d8704b505248d45dcc
Vitoria-Emanuele <vitoriaemanueledesousa@gmail.com>
2026-04-24 15:48:28 -0300

    feat: adiciona pipeline de pre-processamento e formatação de texto no pyspark

--- data_prep.py
+++ data_prep.py
@@ -1,21 +1,43 @@
 import kagglehub
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, initcap
+from pyspark.sql.functions import col, initcap, concat_ws
 
 def iniciar_sessao_spark():
-    print("Iniciando a sessão do PySpark...")
+    print("Iniciando a sessão do PySpark.")
     spark = SparkSession.builder \
         .appName("iFood_Data_Prep") \
         .getOrCreate()
     return spark
 
+
 def carregar_dados_brutos():
-    print("Baixando o dataset do Kaggle...")
+    print("Baixando o dataset do Kaggle.")
     path = kagglehub.dataset_download("benjnb/global-street-food-dataset")
     print(f"Caminho para os arquivos do dataset: {path}")
     
     csv_path = f"{path}/global_street_food.csv"
     return csv_path
+
+def preprocessar_dados(df):
+    """Aplica as regras de limpeza de dados """
+    print("Iniciando o pré-processamento:")
+    
+    # remove linhas duplicadas
+    df_limpo = df.dropDuplicates()
+    
+    # tratar valores nulos preenchendo com "desconhecido" (para strings)
+    df_limpo = df_limpo.fillna("desconhecido")
+    
+    # formata a coluna "Dish Name" como titulo (Initcap)
+    df_limpo = df_limpo.withColumn("Dish Name", initcap(col("Dish Name")))
+    
+    # unir a descrição, metodo de cozimento e ingredientes com um ponto final
+    df_limpo = df_limpo.withColumn(
+        "text_for_embedding", 
+        concat_ws(" ", col("Description"), col("Cooking Method"), col("Ingredients"))
+    )
+    
+    return df_limpo
 
 
 if __name__ == "__main__":
@@ -28,5 +50,8 @@ if __name__ == "__main__":
     # leitura do CSV 
     print("Carregando dados no DataFrame...")
     df_bruto = spark.read.csv(caminho_csv, header=True, inferSchema=True)
+
+    # funcao de limpeza
+    df_processado = preprocessar_dados(df_bruto)
     
-    df_bruto.show(5)
+    df_processado.select("Dish Name", "text_for_embedding").show(5, truncate=False)
